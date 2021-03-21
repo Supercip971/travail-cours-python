299c513a92bdc8d09d60d624335370d6116f3501
supercyp <supercyp@protonmail.com>
2021-03-21 05:03:09 -0400

    [reveal.js] ajout du plugin speakerview

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import Reveal from 'reveal.js';
 import Markdown from 'reveal.js/plugin/markdown/markdown.esm.js';
 import Highlight from 'reveal.js/plugin/highlight/highlight.js';
+import speakerView from 'reveal.js/plugin/notes/notes.js';
 
 let deck = new Reveal({
-    plugins: [Markdown, Highlight]
+    hash: true,
+    plugins: [Markdown, Highlight, speakerView]
 })
+
 deck.initialize();
